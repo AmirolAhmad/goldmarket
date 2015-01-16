1d967b6aaa7788cf5bfb3d14a45b10a5ef5f4aad
AmirolAhmad <9w2pdf@gmail.com>
2015-01-16 16:53:44 +0800

    fixed the auth

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
     :address        => 'smtp.mandrillapp.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => <%= ENV["MANDRILL_USERNAME"] %>,
-    :password       => <%= ENV["MANDRILL_PASSWORD"] %>,
+    :user_name      => ENV["MANDRILL_USERNAME"],
+    :password       => ENV["MANDRILL_PASSWORD"],
     :domain         => 'staging.goldmarket.my',
     :enable_starttls_auto => true
   }
