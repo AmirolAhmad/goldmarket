993c3990627295ec8c88fb6431bc551d941c25fb
AmirolAhmad <9w2pdf@gmail.com>
2015-01-16 15:37:01 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Package.find_or_create_by(
+	name: "ETA7",
+	price: "12,650",
+	landing_date: "02/01/2015"
+)
+
+Package.find_or_create_by(
+	name: "ETA14",
+	price: "11,800",
+	landing_date: "09/01/2015"
+)
+
+Package.find_or_create_by(
+	name: "ETA21",
+	price: "11,200",
+	landing_date: "16/01/2015"
+)
+
+Package.find_or_create_by(
+	name: "ETA30",
+	price: "10,200",
+	landing_date: "26/01/2015"
+)
